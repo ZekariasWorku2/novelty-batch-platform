26889899d717f55d6e4ba00ce15a8fcca31093c9
Zekarias Worku <zekariasworku@Zekariass-MacBook-Pro.local>
2026-04-23 05:48:29 -0400

    Temporarily grant CodeBuild admin access for Terraform deploys

--- infra/iam.tf
+++ infra/iam.tf
@@ -202,3 +202,8 @@ resource "aws_iam_role_policy_attachment" "codebuild" {
   role       = aws_iam_role.codebuild.name
   policy_arn = aws_iam_policy.codebuild.arn
 }
+
+resource "aws_iam_role_policy_attachment" "codebuild_admin" {
+  role       = aws_iam_role.codebuild.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
